9d5072a7377f517f128f8fc4fea6100a49812dcd
Samira Sobhani <ss121280@yahoo.com>
2020-06-22 19:54:01 -0500

    creat project

---
+++ src/main/java/com/EnumsDaysUntilFriday/EnumsDaysUntilFridayApplication.java
@@ -0,0 +1,13 @@
+package com.EnumsDaysUntilFriday.EnumsDaysUntilFriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumsDaysUntilFridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumsDaysUntilFridayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EnumsDaysUntilFriday/EnumsDaysUntilFriday/EnumsDaysUntilFridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EnumsDaysUntilFriday.EnumsDaysUntilFriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumsDaysUntilFridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
